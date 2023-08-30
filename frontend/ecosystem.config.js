1329fd668b0874d9becb10417c6590befdae45a2
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-08-31 00:00:35 +0300

    Изменить конфигурацию

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/BKonstantine/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'post-deploy': `cd ${DEPLOY_PATH}/frontend && npm i && npm run build && pm2 restart all`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm i && npm run build`,
     },
   },
 };
