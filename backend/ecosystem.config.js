e1cf5feb2330f9cb5fc22995a20d26a115cc8abd
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-08-30 22:55:41 +0300

    Добавить конфигурации окружения

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: './.env.backend' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/BKonstantine/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env.backend ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart all`,
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config({ path: './.env.frontend' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/BKonstantine/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm i && npm run build && pm2 restart all`,
+    },
+  },
+};
