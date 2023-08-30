429380382ea9b23c66f2123b5df58183d73cb27b
DIGDES\Buzunov.K <Buzunov.K@digdes.com>
2023-08-30 19:00:12 +0300

    Внести изменения в бэк

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,9 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 app.use(cors({
-  origin: '*',
+  origin: [
+    'https://mesto.front.kbuzunov.nomoredomainsicu.ru',
+  ],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
