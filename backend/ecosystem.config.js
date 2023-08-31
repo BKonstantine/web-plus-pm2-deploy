9d5db1d8b56302a80ab2f18414d597cd3dae7aa3
DIGDES\Buzunov.K <Buzunov.K@digdes.com>
2023-08-31 11:27:16 +0300

    Изменить файлы конфигурации

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './.env.backend' });
+require('dotenv').config({ path: './.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
